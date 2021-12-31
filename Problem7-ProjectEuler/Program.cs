226ed67391c9e57ad082f8b8338039a7a51f2b1d
nikil <nikil@DESKTOP-73MHGNO>
2021-12-30 20:02:49 -0500

    Add project files.

---
+++ Problem7-ProjectEuler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem7_ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            int primeCount = 1;
+            int generalCount = 3;
+
+            while (true)
+            {
+                bool primeFound = true;
+                foreach(int prime in primes)
+                {
+                    if(generalCount % prime == 0)
+                    {
+                        primeFound = false;
+                        break;
+                    }
+                }
+                if(primeFound == true)
+                {
+                    primes.Add(generalCount);
+                    primeCount++;
+                }
+
+                if (primeCount == 10001)
+                    break;
+                generalCount++;
+
+            }
+
+            Console.WriteLine(primes[10000] + " is the 10 001st prime number");
+        }
+    }
+}
